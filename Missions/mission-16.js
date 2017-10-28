48794dafae108a1b8d9a1be46298f9de88928a8a
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2017-10-28 09:35:38 +0800

    Minimised code to meet just enough spec requirement

--- Missions/mission-16.js
+++ Missions/mission-16.js
@@ -33,7 +33,7 @@ var shortname   = "ics";
 //-------------------------------------------------------------------------
 // icsbot
 //-------------------------------------------------------------------------
-// /* Uncomment this when you have set your class name
+
 function icsbot(name){
     Player.call(this, name);
 }
@@ -94,72 +94,7 @@ icsbot.prototype.__act = function(){
     if (!is_empty_list(cur_protectedroom)) {
         // Move to it
         this.moveTo(head(cur_protectedroom));
-    } else {
-        // Find **a** room neighbouring that contains ServiceBot
-        var room_with_svcbots = filter(function(x) {
-                                         var cur = x.getOccupants();
-                                         return !is_empty_list(cur)
-                                                && !is_empty_list(
-                                                     filter(isServiceBot, cur)
-                                                    );
-                                       }, cur_neighbouring);
-        // If indeed there is a neighbouring room that contains ServiceBot
-        if (!is_empty_list(room_with_svcbots)) {
-            // Move to that room
-            this.moveTo(head(room_with_svcbots));
-        } else {
-            // An array containing all possible directions
-            var directions = ["north", "south", "east", "west", "up", "down"];
-            // Pick a direction randomly
-            var cur_dir = directions[math_round(math_random() * 5)];
-            // Keep on picking randomly if we cannot go to that direction
-            // IF ONLY THE SOURCE HAS DO-WHILE CONSTRUCT
-            while (is_empty_list(cur_loc.getExit(cur_dir))) {
-                cur_dir = directions[math_round(math_random() * 5)];
-            }
-            this.go(cur_dir);
-        }
-        
-        // We are in the new room. ATTACK AGAIN!
-        // Get the current location
-        cur_loc = this.getLocation();
-        
-        // Retrieve a list of charged weapons
-        charged_weapons = filter(function(x) {
-                                         return is_instance_of(x, Weapon)
-                                                && !x.isCharging();
-                                     }, this.getPossessions());
-        
-        // Only attack if I have a charged weapon
-        if (!is_empty_list(charged_weapons)) {
-            // Find service bots in the same room
-            svcbots = filter(isServiceBot, cur_loc.getOccupants());
-            // Only attack if there is at least a service bot in the same room
-            if (!is_empty_list(svcbots)) {
-                // Attack a ServiceBot using a charged Weapon
-                this.use(head(charged_weapons), svcbots);
-            } else { }
-        } else { }
-        
-        // Find things not owned by anyone yet
-        cur_things = cur_loc.getThings();
-        // Preventing "Error: undefined at undefined, line undefined"
-        thing = undefined;
-        // Iterate through cur_things to find a keycard
-        while (!is_empty_list(cur_things)) {
-            thing = head(cur_things);
-            // If the current thing is a keycard
-            if (is_instance_of(thing, Keycard)) {
-                // Take it
-                this.take(list(thing));
-                // We have picked up **a** Keycard. Stop iterating.
-                break;
-            } else { }
-            cur_things = tail(cur_things);
-        }
-        
-    }
+    } else { }
 };
 var newPlayer = new icsbot(shortname);
 test_task2(newPlayer);
-// */
