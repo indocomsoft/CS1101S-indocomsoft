ef5333e526efcf8833ff471de83779ca068a470e
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2017-11-01 12:24:21 +0800

    M18: template

---
+++ Missions/mission-18-task-1.js
@@ -0,0 +1,9 @@
+// Source Week 10
+
+/*
+Copy your class from mission 17-2 here, and extend its functionality
+*/
+
+// Uncomment the following to test
+// var newPlayer = new [your class name here](shortname);
+// test_task1(newPlayer);
---
+++ Missions/mission-18-task-2.js
@@ -0,0 +1,9 @@
+// Source Week 10
+
+/*
+Copy your class from the previous task here, and extend its functionality
+*/
+
+// Uncomment the following to test
+// var newPlayer = new [your class name here](shortname);
+// test_task2(newPlayer);
